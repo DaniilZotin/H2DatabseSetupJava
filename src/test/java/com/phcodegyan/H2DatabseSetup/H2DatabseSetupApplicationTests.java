09cf7ce8cb93e47e42307c3a15de751b45189aa9
Daniil Zotin <you@gmail.com>
2023-03-14 11:43:34 +0200

    Delete libraries

--- src/test/java/com/phcodegyan/H2DatabseSetup/H2DatabseSetupApplicationTests.java
+++ src/test/java/com/phcodegyan/H2DatabseSetup/H2DatabseSetupApplicationTests.java
@@ -1,28 +1,23 @@
 package com.phcodegyan.H2DatabseSetup;
 
 import com.phcodegyan.H2DatabseSetup.db.Age;
-import com.phcodegyan.H2DatabseSetup.db.Person;
 import com.phcodegyan.H2DatabseSetup.servises.Servise;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
